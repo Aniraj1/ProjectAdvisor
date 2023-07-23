a94412268848db30cde9400f8ad1cb81ef9b91fc
Shrine <nischalthapa10@hotmail.com>
2023-07-23 09:26:39 +0545

    changed model view for product and order

--- backend/order/admin.py
+++ backend/order/admin.py
@@ -3,10 +3,12 @@ from .models import Order,UserSubscription
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
+    list_display=['id','price','user','created_at','payment_status','payment_mode']
     list_filter = ['price', 'user', 'created_at','payment_status','payment_mode']
     
 @admin.register(UserSubscription)
 class UserSubscriptionAdmin(admin.ModelAdmin):
+    list_display=['can_order','user']
     list_filter= ["can_order",]
     
 # admin.site.register("Order")
--- backend/product/admin.py
+++ backend/product/admin.py
@@ -4,6 +4,7 @@ from . models import Product
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'price', 'user', 'createdAt']
     list_filter = ['price', 'user', 'createdAt']
 
 
